80d9a03828fe2d61896810f3f6950d207e8c5b5a
Joao Lucas Araujo Franca <83269013+jlucasaf@users.noreply.github.com>
2024-04-28 11:24:26 -0300

    feat: novo teste para X
    Novo teste para o algorismo romano X, teste falhou como esperado

--- testa_romanos.cpp
+++ testa_romanos.cpp
@@ -7,6 +7,8 @@ TEST_CASE("Numeros romanos - algarismos unicos", "[romanos]")
     REQUIRE(romanos_para_decimal("I") == 1);
 
     REQUIRE(romanos_para_decimal("V") == 5);
+
+    REQUIRE(romanos_para_decimal("X") == 10);
 }
 
 TEST_CASE("Numeros romanos - algarismos invalidos", "[romanos]")
